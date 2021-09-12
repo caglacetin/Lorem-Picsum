2136980bdd90196d72bad1c829cee64660b156ed
Cagla <cagla.cetin@betbull.com>
2021-09-12 10:10:17 +0300

    Added Loading and Error status for ImageListActivity

--- app/src/main/java/com/caglacetin/lorempicsum/ui/imagelist/ImageListViewState.kt
+++ app/src/main/java/com/caglacetin/lorempicsum/ui/imagelist/ImageListViewState.kt
@@ -1,13 +1,11 @@
 package com.caglacetin.lorempicsum.ui.imagelist
 
-import com.caglacetin.lorempicsum.common.Status
+import com.caglacetin.lorempicsum.common.Resource
 
 data class ImageListViewState(
-  private val status: Status
+  private val status: Resource<Any>
 ) {
-
-  fun isLoading() = status is Status.Loading
-  fun getErrorMessage() = if (status is Status.Error) status.exception else ""
-  fun shouldShowErrorMessage() = status is Status.Error
-
+  fun isLoading() = status is Resource.Loading
+  fun getErrorMessage() = if (status is Resource.DataError) status.exception else ""
+  fun shouldShowErrorMessage() = status is Resource.DataError
 }
---
+++ app/src/test/java/com/caglacetin/lorempicsum/ImageItemViewStateTest.kt
@@ -0,0 +1,6 @@
+package com.caglacetin.lorempicsum
+
+class ImageItemViewStateTest {
+
+
+}
